01511695d4dd6c3f43589651afb061e9cb094b66
ASAD NADEEM <asad9deem@gmail.com>
2026-02-15 22:24:34 +0500

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,11 @@
+print("Welcome to my Python startup 🚀")
+
+name = input("Enter your name: ")
+print(f"Welcome, {name} 😍")
+
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("You will become an AI Engineer in the future 💪")
+else:
+    print("You are on your way to becoming an AI Engineer 🚀")
